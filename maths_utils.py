64aed18e32d0c364f4fffca1b358219db92063cb
kiran151220 <kiranj1504@gmail.com>
2025-07-22 13:54:24 +0530

    Update maths_utils.py

--- maths_utils.py
+++ maths_utils.py
@@ -1,1 +1,15 @@
+def add(a, b):
+  """
+  Add two numbers and returns the result.
 
+  Parameters:
+
+  a(int):First number
+  b(int):Second number
+
+  Returns:
+
+  int:Sum of a and b
+  """
+
+return a+b
